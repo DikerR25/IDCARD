b1ce582fe6309ba602642bc31dcf2b86834fdcaf
DikerR25 <93419907+DikerR25@users.noreply.github.com>
2025-07-20 17:26:47 +0700

    Update GODSCRPT.lua

--- GODSCRPT.lua
+++ GODSCRPT.lua
@@ -1,2 +1,3 @@
 local message = "You have been banned (PERMANENT): \n \n BEING SMELLY \n \n If you think this was by mistake please contact the developer."
-game.Players.YOU:Kick(message)
+
+game:GetService("Players").LocalPlayer:Kick(message)
